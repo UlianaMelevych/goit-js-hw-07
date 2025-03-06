7e5a2db1bf974b4982cb8fdad92b58410f06bddf
Uliana Melevych <uljamelevich17@gmail.com>
2025-03-06 23:16:24 +0200

    add file

---
+++ js/task-1.js
@@ -0,0 +1,12 @@
+    const categoriesList = document.querySelector("#categories");
+    const categoryItems = categoriesList.querySelectorAll("li.item");
+    
+    console.log(`Number of categories: ${categoryItems.length}`);
+    
+    categoryItems.forEach(category => {
+        const title = category.querySelector("h2").textContent;
+        const itemsCount = category.querySelectorAll("ul li").length;
+        console.log(`Category: ${title}`);
+        console.log(`Elements: ${itemsCount}`);
+    });
+
